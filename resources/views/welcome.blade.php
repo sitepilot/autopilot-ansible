5b5820132d09ab27f4dd3e2fd3ab5377245a19a9
Nick Jansen <n.jansen@sitepilot.io>
2020-06-17 22:30:31 +0200

    Add link to monitor status

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -86,6 +86,7 @@
 
                 <div class="links">
                     <a href="{{ url('admin') }}">Admin</a>
+                    <a href="{{ url('status') }}">Status</a>
                 </div>
             </div>
         </div>
