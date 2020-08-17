5d5bd70fc182eca7f3a9400d068e488606b87c73
Nick Jansen <n.jansen@sitepilot.io>
2020-08-17 14:29:56 +0200

    Update python version

--- resources/views/ansible/inventory.blade.php
+++ resources/views/ansible/inventory.blade.php
@@ -1,4 +1,4 @@
 [autopilot]
 @foreach($servers as $server)
-{!! $server->name !!} ansible_user={!! $server->user !!} ansible_host={!! $server->address !!} ansible_port={!! $server->port !!} ansible_port={!! $server->port !!} ansible_ssh_private_key_file={!! $server->keyPath() !!}
+{!! $server->name !!} ansible_user={!! $server->user !!} ansible_host={!! $server->address !!} ansible_port={!! $server->port !!} ansible_port={!! $server->port !!} ansible_ssh_private_key_file={!! $server->keyPath() !!} ansible_python_interpreter=/usr/bin/python3
 @endforeach
